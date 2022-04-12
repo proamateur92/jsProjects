8a20ed57dbdfbb12d2d9e435f9bb6c22b9fa6ede
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-04-12 14:30:13 +0900

    updated drum game

--- projects/drumGame/main.js
+++ projects/drumGame/main.js
@@ -3,6 +3,7 @@ function playSound(e) {
   const audio = document.querySelector(`audio[data-key='${keyCode}']`);
   const key = document.querySelector(`.key[data-key='${keyCode}']`);
 
+  // 타이핑된 값으로부터 가져온 audio객체가 null이면 return
   if (!audio) return;
 
   audio.currentTime = 0;
@@ -11,8 +12,10 @@ function playSound(e) {
 }
 
 function removeTransition(e) {
+  // transition이 종료될 때 변경된 property값이 transform이 아니면 return
   if (e.propertyName !== 'transform') return;
-  console.log(e.propertyName);
+
+  // removeTransition 함수가 호출되는 시점의 key객체의 class이름 제거
   this.classList.remove('playing');
 }
 
