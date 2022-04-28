7398aed551138799a6e870f882881187b92c6f24
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-04-29 02:32:13 +0900

    refactoring pingpong score keeper project

--- projects/pingPong/pingPong.js
+++ projects/pingPong/pingPong.js
@@ -1,61 +1,65 @@
-let p1 = 0;
-let user2 = 0;
-// 라운드 수 5로 초기값 설정
-let round = 5;
+// 듀스 기능 추가
+// 플레이어 추가
+// 효과 추가 등
+// 기능 개선 필요
+
+const p1 = {
+  score: 0,
+  button: document.querySelector('.player1-btn'),
+  display: document.querySelector('.score-board > span:first-child'),
+};
+
+const p2 = {
+  score: 0,
+  button: document.querySelector('.player2-btn'),
+  display: document.querySelector('.score-board > span:last-child'),
+};
+
+let winningPoint = 5;
 let isGameOver = false;
 
 const scoreBoard = document.querySelector('.score-board');
 const roundInput = document.querySelector('#round');
-const p1Btn = document.querySelector('.player1-btn');
-const p2Btn = document.querySelector('.player2-btn');
 const resetBtn = document.querySelector('.reset-btn');
-const p1Score = document.querySelector('.score-board > span:first-child');
-const p2Score = document.querySelector('.score-board > span:last-child');
 
 function roundChange() {
-  round = parseInt(this.value);
+  winningPoint = parseInt(this.value);
   reset();
 }
 
 const reset = () => {
   isGameOver = false;
-  p1Btn.disabled = false;
-  p2Btn.disabled = false;
-  p1 = 0;
-  user2 = 0;
-  p1Score.classList.remove('has-text-success', 'has-text-danger');
-  p2Score.classList.remove('has-text-success', 'has-text-danger');
-  p1Score.innerText = 0;
-  p2Score.innerText = 0;
-};
 
-const scoreUp = event => {
-  const player = event.target.dataset.num;
-
-  if (!isGameOver) {
-    player === '1' ? p1++ : user2++;
-
-    p1Score.innerText = p1;
-    p2Score.innerText = user2;
-
-    if (p1 >= round || user2 >= round) {
-      isGameOver = true;
-      // 최종 스코어 도달 시 승리 녹색 글씨, 패배 빨간 글씨
-      // 버튼 2개 잠김
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
-      if (p1 > user2) {
-        p1Score.classList.add('has-text-success');
-        p2Score.classList.add('has-text-danger');
-      } else {
-        p1Score.classList.add('has-text-danger');
-        p2Score.classList.add('has-text-success');
-      }
-    }
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.button.disabled = false;
+    p.display.classList.remove('has-text-success', 'has-text-danger');
+    p.display.innerText = 0;
   }
 };
 
-p1Btn.addEventListener('click', scoreUp);
-p2Btn.addEventListener('click', scoreUp);
+const updateScore = (player, opponent) => {
+  if (!isGameOver) {
+    player.score++;
+    console.log(1);
+    console.log(player.score);
+    if (player.score === winningPoint) {
+      isGameOver = true;
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+      player.display.classList.add('has-text-success');
+      opponent.display.classList.add('has-text-danger');
+    }
+    player.display.innerText = player.score;
+  }
+};
+
+p1.button.addEventListener('click', function () {
+  updateScore(p1, p2);
+});
+p2.button.addEventListener('click', function () {
+  updateScore(p2, p1);
+});
+
 resetBtn.addEventListener('click', reset);
 roundInput.addEventListener('change', roundChange);
